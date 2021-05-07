cb4364ebbaf38f405510a6a440a15f608ffef0ca
zonteur <70370396+zonteur@users.noreply.github.com>
2021-05-07 15:01:59 +0200

    change to php

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php 
+include 'parts/header.php';
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS Bootstrap  version 4.6 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+
+    <!-- mes styles CSS -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>Belleza</title>
+</head>
