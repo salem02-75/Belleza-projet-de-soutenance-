67c74e1dd50ed77982b3450c5d3f4c11d275c1fd
salima02-75 <salem-salima75@hotmail.fr>
2021-05-07 15:40:36 +0200

    font familly

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
  
 # le guitube de SALIMA est :   salem02-75
 # le guitube de GUILLUME est :   ?
-# le guitube de AIDA est :       ?    
+# le guitube de AIDA est :       ?   
+# les police :
+# font-family: 'Belleza', sans-serif;
+# font-family: 'Didact Gothic', sans-serif;
--- parts/header.php
+++ parts/header.php
@@ -5,7 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <!-- les fontes -->
+    <!-- link font belleza -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet">
+ <!--link font Dicact Gothic  -->
+ <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Didact+Gothic&display=swap" rel="stylesheet">
+ 
     <!-- CSS Bootstrap  version 4.6 -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
