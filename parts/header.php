f71fc9f4ab7e73d46eb17f731f976b1795681d72
salima02-75 <salem-salima75@hotmail.fr>
2021-05-07 15:49:41 +0200

    font css

--- parts/header.php
+++ parts/header.php
@@ -5,7 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <!-- les fontes -->
+
     <!-- link font belleza -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
 <link href="https://fonts.googleapis.com/css2?family=Belleza&display=swap" rel="stylesheet">
