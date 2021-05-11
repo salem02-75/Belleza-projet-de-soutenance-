d85cd751627b179fdc84ce00eda71772127f09e1
aida <salemaida@live.fr>
2021-05-11 16:03:37 +0200

    ff

---
+++ contact.php
@@ -0,0 +1,60 @@
+<?php
+include 'parts/header.php';
+?>
+
+<main>
+    <h1>Formulaire de contact</h1>
+
+    <section>
+
+        <section>
+            <div>
+                <img src="" alt="">
+                <h2>nom du proffesionnel</h2>
+            </div>
+            <div>
+                <p>etoiles</p>
+                <p>ville</p>
+            </div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, earum unde expedita assumenda harum saepe ex ad. Non voluptate quod error adipisci iure asperiores sapiente?</p>
+
+            <section>
+                <p>tableau dates rdv </p>
+                <button>reserver</button>
+            </section>
+        </section>
+
+        <section>
+            <img src="" alt="">
+        </section>
+
+    </section>
+
+    {# parti deroulante qui saffiche lorsque l'on appuis sur la fleche ou sur envoyer #}
+
+    <section>
+        <button>></button>
+
+        <div>
+            <div>
+                <h3>name </h3>
+                <h3>littlename</h3>
+            </div>
+            <div>
+                mail
+            </div>
+            <div>
+                content
+            </div>
+        </div>
+
+        <button>button</button>
+    </section>
+
+</main>
+
+
+
+<?php
+include 'parts/footer.php';
+?>
