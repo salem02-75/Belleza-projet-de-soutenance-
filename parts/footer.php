4590143123e58d6787854e99db315f68db8a6290
zonteur <70370396+zonteur@users.noreply.github.com>
2021-05-11 15:18:18 +0200

    Footer

---
+++ parts/footer.php
@@ -0,0 +1,80 @@
+    <!-- ========================================= -->
+    <!-- footer start  -->
+    <!-- ========================================= -->
+    <div class="footer">
+        <img class="logo-bg" src="/img/logo.svg" alt="">
+        <!-- bg-dark -->
+        <div class="container bloc-footer bg-dark">
+            <div class="row">
+                <!------ col 1 paje contacte ------->
+                <div class="col-xl-4 col-md-4 col-sm-12 d-flex justify-content-center align-items-center">
+                    <a href="#" class="text-light">Nous-contacter</a>
+                    <hr class="hr-right">
+                </div>
+                <!------ col 2  Réseaux sociaux------->
+                <div
+                    class="col-xl-4 col-md-4 col-sm-12  text-light d-flex flex-column justify-content-center align-items-center">
+                    <!-- <h3>Suivez-nous</h3> -->
+                    <!-------------------------------------------------------------------------------------------------------------->
+                    <!-- un h2 + P sont  disply -none en formaat  xl et md -->
+                    <!-- <div>
+                        <h2>Toujours au contact avec la beauté</h2>
+                        <p>En nous suivant sur Instagram, Facebook ou twitteI</p>
+                    </div> -->
+                    <!-------------------------------------------------------------------------------------------------------------->
+                    <!-- include  les réseaux sociaux  -->
+                    <div class="container">
+                        <div class="bloc footer-medias">
+                            <div class="liste-media  d-flex justify-content-around align-items-center">
+                                <a href="#">
+                                    <i class="fab fa-facebook-f"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fab fa-instagram"></i>
+                                </a>
+                                <a href="#">
+                                    <i class="fab fa-twitter"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <hr class="hr-right">
+                </div>
+                <!----- col 3 btn espace pro-------->
+                <div class="col-xl-4 col-md-4  col-sm-12 d-flex justify-content-center align-items-center ">
+                    <!-- metre le #lien  de la page pro -->
+                    <button class="btn btn-outline-success"><a href="#" class="text-light">Déposer une
+                            annonce</a></button>
+                    <!-- <div class="ligne"></div> -->
+                </div>
+            </div>
+            <hr class="hr-top">
+            <!--------------------------- 2eme patie du footer avec un bcg image en z-index 2 --------------------------->
+            <div class="container  py-xl-5 py-md-5">
+                <div class="row">
+                    <!------ col 1 Menu------->
+                    <div
+                        class="col-xl-4 col-md-4  col-sm-12  d-flex flex-column justify-content-center align-items-center">
+                        <!-- rajouter les liens # -->
+                        <a href="" class="text-light pb-xl-3">Coiffure</a>
+                        <a href="" class="text-light pb-xl-3">Esthétique</a>
+                        <a href="" class="text-light pb-xl-3">Massage</a>
+                    </div>
+                    <!------ col 2  AFQ/Avis-Mention légale------->
+                    <div
+                        class="col-xl-4 col-md-4  col-sm-12 d-flex flex-column justify-content-center align-items-center">
+                        <!-- rajouter les liens # -->
+                        <a href="" class="text-light pb-xl-3">Mention légal</a>
+                        <a href="" class="text-light pb-xl-3">FAQ/Aide</a>
+                        <a href="" class="text-light pb-xl-3">Les avis</a>
+                    </div>
+                    <!------ col 3 Newsletter ------->
+                    <div class="col-xl-4 col-md-4  col-sm-12 d-flex  justify-content-center ">
+                        <!-- mettre le bon le #lien vers la newletter -->
+                        <a href="nesletter" class="text-light">Newsletter</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--  fin de footer -->
