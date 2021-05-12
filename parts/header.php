90ac646d8da6c1feea21b556d41e2dda79a2bd6f
zonteur <70370396+zonteur@users.noreply.github.com>
2021-05-12 18:58:00 +0200

    Update header.php

--- parts/header.php
+++ parts/header.php
@@ -26,13 +26,6 @@
     <!-- mes styles CSS -->
     <link rel="stylesheet" href="style/style.css">
 
-     <!-- icons (éseaux sociaux)-->
-     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    
-
-    <!-- icons (éseaux sociaux)-->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-
     <title> <?php $title_belleza ?> </title>
     </head>
 
