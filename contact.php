3a5b1daf9b1f87471d682d75d4a2c991ba854735
aida <salemaida@live.fr>
2021-05-17 00:12:16 +0200

    o

--- contact.php
+++ contact.php
@@ -1,7 +1,11 @@
 <?php
 include 'parts/header.php';
 ?>
+
+
 <php
+
+
 
 $VotreAdresseMail="votreemail@votresite.tld";
 if(isset($_POST['envoyer'])) { 
@@ -20,13 +24,13 @@ if(isset($_POST['envoyer'])) {
                 }else{
                     $Entetes = "MIME-Version: 1.0\r\n";
                     $Entetes .= "Content-type: text/html; charset=UTF-8\r\n";
-                    $Entetes .= "From: Nom de votre site <".$_POST['mail'].">\r\n";
-                    $Entetes .= "Reply-To: Nom de votre site <".$_POST['mail'].">\r\n";
-                    //on prépare les champs:
+                    $Entetes .= "From: Nom de votre site <".$_POST['mail'] ;
+                    $Entetes .= "";
+                   
                     $Mail=$_POST['mail']; 
                     $Sujet='=?UTF-8?B?'.base64_encode($_POST['sujet']).'?=';//Cet encodage (base64_encode) est fait pour permettre aux informations binaires d'être manipulées par les systèmes qui ne gèrent pas correctement les 8 bits (=?UTF-8?B? est une norme afin de transmettre correctement les caractères de la chaine)
                     $Message=htmlentities($_POST['message'],ENT_QUOTES,"UTF-8");//htmlentities() converti tous les accents en entités HTML, ENT_QUOTES Convertit en + les guillemets doubles et les guillemets simples, en entités HTML
-                    //en fin, on envoi le mail
+                    
                     if(mail($VotreAdresseMail,$Sujet,nl2br($Message),$Entetes)){//la fonction nl2br permet de conserver les sauts de ligne et la fonction base64_encode de conserver les accents dans le titre
                         echo "Le mail à été envoyé avec succès!";
                     } else {
@@ -39,6 +43,10 @@ if(isset($_POST['envoyer'])) {
 }
 ?>
 
+<head>
+
+
+<main>
 
 <form action="contact.php" method="post">
     Mail: <input type="text" name="mail" value="" />
@@ -48,7 +56,15 @@ if(isset($_POST['envoyer'])) {
     Message: <textarea name="message" cols="40" rows="20"></textarea>
     <br />
     <input type="submit" name="envoyer" value="Envoyer" />
+
+
 </form>
+</main>
+</head>
+
+
+
+
 
 
 
