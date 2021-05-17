a66f408f1b7c9878a56ac51886c9625ed9a6c27b
zonteur <70370396+zonteur@users.noreply.github.com>
2021-05-17 10:42:58 +0200

    Ajout d'un bouton

--- CoiffureOffres.php
+++ CoiffureOffres.php
@@ -5,42 +5,33 @@ include 'parts/header.php';
   <div class="row d-flex justify-content-center mt-5">
 
 
-
-
+{# template pour une future boucle php : #}
     <!-- Carte Offre -->
-    <div class="card mb-3 mb-5" style="max-width: 90vh;">
+    <div class="card mb-3 mb-5" style="max-width: 135vh;">
       <div class="row g-0">
-        <div class="col-md-4 overflow-hidden taille_img_service_offres">
+
+        <div class="col-4 overflow-hidden taille_img_service_offres">
           <!-- IMG -->
-            <img src="https://picsum.photos/330/370" alt="ALEATOIR">
+          <img src="https://picsum.photos/330/370" alt="ALEATOIR">
         </div>
-        <div class="col-md-8">
+
+        <div class="col-6">
           <div class="card-body">
             <h5 class="card-title">Card title</h5>
             <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
           </div>
         </div>
-      </div>
-    </div>
-    <!-- Fin carte Offre -->
-    <div class="card mb-3 mb-5" style="max-width: 90vh;">
-      <div class="row g-0">
-        <div class="col-md-4 taille_img_service_offres">
-          <!-- IMG -->
-            <img src="https://picsum.photos/430/270" alt="">
-        </div>
-        <div class="col-md-8">
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+
+      {# Ici besoin d'un margin right #}
+        <div class="col-2 d-flex align-items-center ">
+          <div>
+            <a class="button_belleza" href="CoiffureOffres.php">Consulter</a>
           </div>
+
         </div>
       </div>
     </div>
-  </div>
-</div>
 
 
 
