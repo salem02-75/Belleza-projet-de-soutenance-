eebfd2975ee2832627edc6c8ec578cebbe971b84
zonteur <70370396+zonteur@users.noreply.github.com>
2021-05-25 19:22:00 +0200

    banniere v2

--- functions/tri.php
+++ functions/tri.php
@@ -1,4 +1,25 @@
 <!-- fonction tri d'index -->
 <?php
+if ( ! empty($_GET['page']))
+{
+    $order = '';
 
+    if ( ! empty($_POST['date']))
+    {
+        $date = html($_POST['date']);
+
+        if ($date == 'DESC')
+        {
+            $order .= " ORDER BY DESC";
+        }
+        else if ($date == 'ASC')
+        {
+            $order .= " ORDER BY ASC";
+        }
+    }
+     
+  }
+
+      
+?>
 ?>
