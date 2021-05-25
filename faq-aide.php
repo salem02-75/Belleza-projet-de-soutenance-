1c48297f9e6aa373114f5db9e959e988580d0739
salima02-75 <salem-salima75@hotmail.fr>
2021-05-25 04:57:14 +0200

    footer-light

--- faq-aide.php
+++ faq-aide.php
@@ -1,11 +1,160 @@
 <?php
 $page = 'Belleza FAQ';
-include '../parts/header.php';
+include 'parts/header.php';
 ?>
 
+<section id="faq" class="my-150">
+            <div class="container ">
+                <div class="row">
+                    <div class="col-md-8 offset-md-2">
+
+                        <h1 class="d-flex justify-content-center">Consultez la Foir aux questions</h1>
+
+                        <div class="accordion mt-5" id="accordionExample">
+                            <!-- card 1 de la FAQ -->
+                            <div class="card">
+
+                                <div class="card-header" id="headingOne">
+                                    <!-- titre 1 -->
+                                    <h2 class="claerfix my-2">
+                                        <a class="btn btn-link text-decoration-none font-weight-bold text-dark" data-toggle="collapse" data-target="#collapseOne"
+                                            aria-expanded="true" aria-controls="collapseOne">
+                                            Je suis un client<i class="fa fa-angle-down ml-3"></i>
+                                        </a>
+                                    </h2>
+                                </div>
+                                <!-- text 1 -->
+                                <div id="collapseOne" class="collapse" aria-labelledby="headingOne"
+                                    data-parent="#accordionExample">
+                                    <div class="card-body my-3 text-secondary">
+                                        BELLEZA, ipsum dolor sit amet consectetur adipisicing elit. Nulla doloremque optio
+                                        maxime illo sunt quaerat enim facere tenetur excepturi? Libero veniam magnam
+                                        iusto cumque rem corporis nam, quas ab cupiditate commodi saepe eius est
+                                        exercitationem aliquid error fugit ea debitis quidem voluptate odio. Nemo
+                                        nesciunt, laudantium illum magnam temporibus dolor?
+                                    </div>
+                                </div>
+
+                            </div><!-- card-1 fin -->
+
+                             <!-- card 2 de la FAQ -->
+                             <div class="card">
+
+                                <div class="card-header" id="headingTwo">
+                                    <!-- titre 2 -->
+                                    <h2 class="claerfix my-2">
+                                        <a class="btn btn-link text-decoration-none font-weight-bold text-dark" data-toggle="collapse" data-target="#collapseTwo"
+                                            aria-expanded="true" aria-controls="collapseTwo">
+                                            Je suis un professionel
+                                             <i class="fa fa-angle-down ml-3"></i>
+                                        </a>
+                                    </h2>
+                                </div>
+                                <!-- text 2 -->
+                                <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo"
+                                    data-parent="#accordionExample">
+                                    <div class="card-body my-3 text-secondary">
+                                        BELLEZA, ipsum dolor sit amet consectetur adipisicing elit. Nulla doloremque optio
+                                        maxime illo sunt quaerat enim facere tenetur excepturi? Libero veniam magnam
+                                        iusto cumque rem corporis nam, quas ab cupiditate commodi saepe eius est
+                                        exercitationem aliquid error fugit ea debitis quidem voluptate odio. Nemo
+                                        nesciunt, laudantium illum magnam temporibus dolor?
+                                    </div>
+                                </div>
+
+                            </div><!-- card-2 fin -->
+
+                             <!-- card 3 de la FAQ -->
+                             <div class="card">
+
+                                <div class="card-header" id="headingThree">
+                                    <h2 class="claerfix my-2">
+                                        <a class="btn btn-link text-decoration-none font-weight-bold text-dark" data-toggle="collapse" data-target="#collapseThree"
+                                            aria-expanded="true" aria-controls="collapseThree">
+                                            Partonarias<i class="fa fa-angle-down ml-3"></i>
+                                        </a>
+                                    </h2>
+                                </div>
+                                <!-- text 3 -->
+                                <div id="collapseThree" class="collapse" aria-labelledby="headingThree"
+                                    data-parent="#accordionExample">
+                                    <div class="card-body my-3 text-secondary">
+                                        BELLEZA, ipsum dolor sit amet consectetur adipisicing elit. Nulla doloremque optio
+                                        maxime illo sunt quaerat enim facere tenetur excepturi? Libero veniam magnam
+                                        iusto cumque rem corporis nam, quas ab cupiditate commodi saepe eius est
+                                        exercitationem aliquid error fugit ea debitis quidem voluptate odio. Nemo
+                                        nesciunt, laudantium illum magnam temporibus dolor?
+                                    </div>
+                                </div>
+
+                            </div><!-- card-3 -fin -->
+
+                             
+                            <!-- card 4 de la FAQ -->
+                            <div class="card">
+
+                                <div class="card-header" id="headingFour">
+                                    <!-- titre 4 -->
+                                    <h2 class="claerfix my-2">
+                                        <a class="btn btn-link text-decoration-none font-weight-bold text-dark" data-toggle="collapse" data-target="#collapseFour"
+                                            aria-expanded="true" aria-controls="collapseOne">
+                                           Coronavirus et Messures sanitaires<i class="fa fa-angle-down ml-2"></i>
+                                        </a>
+                                    </h2>
+                                </div>
+                                <!-- text 4 -->
+                                <div id="collapseFour" class="collapse" aria-labelledby="headingFour"
+                                    data-parent="#accordionExample">
+                                    <div class="card-body my-3 text-secondary">
+                                        BELLEZA, ipsum dolor sit amet consectetur adipisicing elit. Nulla doloremque optio
+                                        maxime illo sunt quaerat enim facere tenetur excepturi? Libero veniam magnam
+                                        iusto cumque rem corporis nam, quas ab cupiditate commodi saepe eius est
+                                        exercitationem aliquid error fugit ea debitis quidem voluptate odio. Nemo
+                                        nesciunt, laudantium illum magnam temporibus dolor?
+                                    </div>
+                                </div>
+
+                            </div><!-- card 4-fin -->
+
+                            <!-- card 5 de la FAQ -->
+                            <div class="card">
+
+                                <div class="card-header" id="headingFive">
+                                    <!-- titre 5 -->
+                                    <h2 class="claerfix my-2">
+                                        <a class="btn btn-link text-decoration-none font-weight-bold text-dark" data-toggle="collapse" data-target="#collapseFive"
+                                            aria-expanded="true" aria-controls="collapseOne">
+                                           Les cookies<i class="fa fa-angle-down ml-3"></i>
+                                        </a>
+                                    </h2>
+                                </div>
+                                <!-- text 5 -->
+                                <div id="collapseFive" class="collapse" aria-labelledby="headingFive"
+                                    data-parent="#accordionExample">
+                                    <div class="card-body my-3 text-secondary">
+                                        BELLEZA, ipsum dolor sit amet consectetur adipisicing elit. Nulla doloremque optio
+                                        maxime illo sunt quaerat enim facere tenetur excepturi? Libero veniam magnam
+                                        iusto cumque rem corporis nam, quas ab cupiditate commodi saepe eius est
+                                        exercitationem aliquid error fugit ea debitis quidem voluptate odio. Nemo
+                                        nesciunt, laudantium illum magnam temporibus dolor?
+                                    </div>
+                                </div>
+
+                            </div><!-- card 5 -fin -->
+
+                            
+                           
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </section>
 
 
 
 <?php
-include '../parts/footer.php';
+include 'parts/footer-light.php';
 ?>
