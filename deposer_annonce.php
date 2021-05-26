ebe0f92e0e40840987a8a15985c0bbee4e4247fb
zonteur <70370396+zonteur@users.noreply.github.com>
2021-05-26 16:33:46 +0200

    Esthetique page: deposer une annonce et envoi mail

--- deposer_annonce.php
+++ deposer_annonce.php
@@ -4,11 +4,13 @@ $page = 'Belleza Deposer une annonce';
 include 'parts/header.php';
 include 'functions/connect.php';
 ?>
-
-
+<!-- Titre -->
+<h4 class="text-center mt-5 pt-3">Deposez une annonce</h4>
 <!-- Selection de la base de donne -->
-<div class="bg-white w-75 m-auto p-4 my-5">
+<div class="bg-white w-75 ml-auto mr-auto  pl-5 pr-5 mt-5 ">
+  <label for="nom_societe" class="form-label">Le secteur d'activité</label>
   <form class="input-group mb-3" method="post" action="traitement.php">
+
     <select class="custom-select" id="inputGroupSelect02">
       <option selected>Selectionner le service proposer</option>
       <option value="<?php echo $services = "coiffures" ?>">Coiffures</option>
@@ -31,32 +33,37 @@ $res = request_sql($dbh, $sql);
 ?>
 
 
-<main>
-  <div class="bg-white w-75 m-auto p-4 my-5">
-    <?php if (isset($_POST["nom_societe"]) && $done) : ?>
-      <p class="m-3">Le post de l'entreprise <?php echo $_POST["nom_societe"]; ?> a bien été ajouter</p>
-    <?php elseif (isset($_POST["nom_societe"]) && !$done) : ?>
-      <p>Le post de l'entreprise <?php echo var_dump($_POST["nom_societe"]) ?> n'a pas été ajouter </p>
-    <?php endif ?>
 
-    <form action="./deposer_annonce.php" method="POST">
+<div class="bg-white w-75 m-auto p-4 my-5">
 
-      <!-- nom de la societe -->
-      <input class="form-control p-1 m-3" type="text" name="nom_societe" id="nom_societe" placeholder="Nom de l’entreprise" required>
 
-      <!-- photo-->
-      <input class="form-control p-1 m-3" type="text" name="photo" id="photo" placeholder="Lien de la photo" required>
+  <form action="./deposer_annonce.php" method="POST">
+
+    <!-- nom de la societe -->
+    <div class="p-1 m-3">
+      <label for="nom_societe" class="form-label">Nom de l'entreprise</label>
+      <input class="form-control" type="text" name="nom_societe" id="nom_societe" placeholder="Nom de l’entreprise" required>
+    </div>
+
+
+    <!-- photo-->
+    <div class="p-1 m-3">
+      <label for="photo" class="form-label">Photo</label>
+      <input class="form-control" type="text" name="photo" id="photo" placeholder="Lien de la photo" required>
       <small class="text-muted m-3">Nécessite un hebergement de la photo au préalable</small>
+    </div>
 
-      <!-- description -->
-      <div class="p-1 m-3">
-        <label for="description" class="form-label">Description</label>
-        <textarea class="form-control" name="description" id="description" rows="3"></textarea>
-      </div>
 
-      <!-- localisation -->
-      <label for="localisation" class="form-label p-1 m-3">Ville</label>
-      <input class="form-control m-3" list="datalistOptions" name="localisation" id="localisation" placeholder="Ville, PAYS">
+    <!-- description -->
+    <div class="p-1 m-3">
+      <label for="description" class="form-label">Description</label>
+      <textarea class="form-control" name="description" id="description" rows="3"></textarea>
+    </div>
+
+    <!-- localisation -->
+    <div class="p-1 m-3">
+      <label for="localisation" class="form-label ">Ville</label>
+      <input class="form-control " list="datalistOptions" name="localisation" id="localisation" placeholder="Ville, PAYS">
       <datalist id="datalistOptions">
         <option value="Paris, France">
         <option value="Marseille, France">
@@ -67,19 +74,32 @@ $res = request_sql($dbh, $sql);
         <option value="Nice, France">
         <option value="Montpellier, France">
       </datalist>
+    </div>
 
-      <!-- email -->
-      <div class="p-1 m-3">
-        <label for="email" class="form-label">Email address</label>
-        <input type="text" class="form-control" name="email" id="email">
-      </div>
 
-      <!-- submit -->
+    <!-- email -->
+    <div class="p-1 m-3">
+      <label for="email" class="form-label">Email address</label>
+      <input type="email" class="form-control" name="email" id="email" placeholder="name@example.com">
+    </div>
+
+    <?php if (isset($_POST["nom_societe"]) && $done) : ?>
+      <p class="m-3">Le post de l'entreprise <?php echo $_POST["nom_societe"]; ?> a bien été ajouter</p>
+    <?php elseif (isset($_POST["nom_societe"]) && !$done) : ?>
+      <p>Le post de l'entreprise <?php echo var_dump($_POST["nom_societe"]) ?> n'a pas été ajouter </p>
+    <?php endif ?>
+    
+    <!-- submit -->
+    <div class="p-1 m-3">
       <a href="./index.php" class="btn btn-warning  my-4">annuler</a>
       <button type="submit" class="btn btn-primary float-end my-4">envoyer</button>
-    </form>
-  </div>
-</main>
+    </div>
+
+
+
+  </form>
+</div>
+
 
 <?php
 include 'parts/footer.php';
