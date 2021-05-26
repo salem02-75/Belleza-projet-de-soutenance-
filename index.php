e31adc0adafb84e69ae6aec55cbcfd24f7da0d35
zonteur <70370396+zonteur@users.noreply.github.com>
2021-05-27 01:59:21 +0200

    correctif color

--- index.php
+++ index.php
@@ -117,7 +117,7 @@ $res = request_sql($dbh, $sql);
     <!-- avis  en format Sm-->
     <!-- ========================================= -->
     <div class="avis-sm py-4">
-        <div class="bg-danger py-4 text-center ">
+        <div class="color_red_belleza py-4 text-center ">
             <h2 class=" font-weight-bolder py-3">Ils nous ont fait confiance</h2>
             <p class="text-white">Nos profesionnels nous ont fait confiance, et leurs clients ont adoré</p>
             <p class="text-white">Vous êtes curieux 🧐 de voir leurs avis, c'est par <a href="avis.php" class="text-avis-sm font-weight-bolder">içi</a></p>
