f23c0e6a13829d5b2bdb56c5971314d92b8e8f45
salima02-75 <salem-salima75@hotmail.fr>
2021-05-27 02:58:08 +0200

    avis
    j'ai remis le balisse de la section

--- nousContacter.php
+++ nousContacter.php
@@ -21,8 +21,6 @@ include 'parts/header.php';
             <div class="container  py-5">
 
                 <h1 class="titre-Contact text-center py-4 font-weight-bold ">Contactez-nous</h1>
-
-
                 <div class="row contact-body mt-5">
                     <div class="col-md-6 pl-3 my-5 ">
                             <h3 class="font-weight-bold pb-2 ">Avant de nous consulter,</h3>
@@ -50,12 +48,11 @@ include 'parts/header.php';
 
                             </div><!-- medias-->
 
-                        </div><!-- contact-media-->
-                    </div>
+                    </div><!-- contact-media-->
+                    <!-- </div> -->
                 </div> <!-- row- nous-contacter fin -->
 
                 <div class="row ">
-
                     <div class="col-md-6 mt-5 pl-4 ">
                         <h3 class="font-weight-bold pb-2 ">Par e-mail</h3>
                         <div class="contact-text">
@@ -73,17 +70,17 @@ include 'parts/header.php';
                             </div>
                                 <form action="contacter_traitement.php" method="POST">
                                     <div class="form-group">
-                                         <div class="py-3 ">
-                                        <input type="text" name="surname" placeholder="Nom" autocomplete="off" class="form-control " />
+                                        <div class="py-3 ">
+                                            <input type="text" name="surname" placeholder="Nom" autocomplete="off" class="form-control " />
                                         </div>
                                         <div class="py-3">
-                                             <input type="text" name="firstname" placeholder="Prénom" autocomplete="off" class="form-control " />
+                                            <input type="text" name="firstname" placeholder="Prénom" autocomplete="off" class="form-control " />
                                         </div>
                                          <div class="py-3">
                                              <input type="email" name="email" placeholder="Email" autocomplete="off" class="form-control " />
                                         </div>
                                         <div class="py-3">
-                                             <textarea rows="10" name="message" placeholder="Envoyez votre message à bellezafrance.contact@gmail.com" class="form-control "></textarea>
+                                            <textarea rows="10" name="message" placeholder="Envoyez votre message à bellezafrance.contact@gmail.com" class="form-control "></textarea>
                                         </div>
                                            
                                         <button type="submit" class="btn btn-lg btn-primary my-3 ">Envoyer</button>
@@ -105,7 +102,7 @@ include 'parts/header.php';
 
 
 
-    </div>
+    </section>
 
 <?php
 include 'parts/footer.php';
