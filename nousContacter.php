303e183848cba5869129c6d0a22eaa83e152c624
salima02-75 <salem-salima75@hotmail.fr>
2021-05-27 02:51:23 +0200

    contacter nous

--- nousContacter.php
+++ nousContacter.php
@@ -15,31 +15,27 @@ include 'parts/header.php';
 
 
     <section class="container  my-5" id="nous-contacter">
-        <section class="  my-5" id="Nous-contacter" action="contacter_traitement.php" method="post">
+        <section class="  mx-2 my-5" id="Nous-contacter" action="contacter_traitement.php" method="post">
 
 
             <div class="container  py-5">
 
-                <h1 class="text-center titre-Contact py-4 font-weight-bold ">Contactez-nous</h1>
+                <h1 class="titre-Contact text-center py-4 font-weight-bold ">Contactez-nous</h1>
 
 
                 <div class="row contact-body mt-5">
                     <div class="col-md-6 pl-3 my-5 ">
-                        <div>
                             <h3 class="font-weight-bold pb-2 ">Avant de nous consulter,</h3>
                             <p>Pensez à voir notre liste de <span><a href="faq-aide.php" class="text-decoration-none">FAQ</a></span></p>
                             <p>Vous pouvez trouvez des avis de nos Pro et leurs fidèles</p>
                             <a class="btn btn-outline my-3" href="avis.php" role="button">les avis de nos
                                 clients</a>
-                        </div>
-
                     </div>
-                    <div class="col-md-4 offset-md-2   my-5 ">
+                    
+                    <div class="col-md-6  my-5 ">
                             <h3 class="font-weight-bold pb-2 ">Via nos réseaux sociaux</h3>
-                            <div class="liste-medias    ">
-                                <!-- <div  class=" "> -->
-                                <!-- <span class=" d-flex border-right "></span> -->
-                                <!-- <div class=" d-flex  "> -->
+                            <div class="liste-medias ">
+                             
                                 <a href="https://www.facebook.com/belleza.france" class="text-decoration-none">
                                     <i class="fab fa-facebook-f pl-5 "></i>
                                 </a>
@@ -51,9 +47,6 @@ include 'parts/header.php';
                                 <a href="https://twitter.com/belleza42020217" class="text-decoration-none">
                                     <i class="fab fa-twitter "></i>
                                 </a>
-                                <!-- </div> -->
-                                <!-- <span class=" border-left "></span> -->
-                                <!-- </div> -->
 
                             </div><!-- medias-->
 
@@ -63,7 +56,7 @@ include 'parts/header.php';
 
                 <div class="row ">
 
-                    <div class="col-md-6 mt-5 ">
+                    <div class="col-md-6 mt-5 pl-4 ">
                         <h3 class="font-weight-bold pb-2 ">Par e-mail</h3>
                         <div class="contact-text">
                             <p>Vous avez un question concernat l'inscription.</p>
@@ -72,36 +65,30 @@ include 'parts/header.php';
                             </p>
                         </div><!-- fin col-email  -->
                     </div>
-                    <div class="  col-md-6  mt-5 ">
-
-
+                    <div class="col-md-6  mt-5 ">
                         <!-- Création du formulaire de contact post -->
                         <div class="container bg-danger">
-                            <div class="d-flex justify-content-center">
-                                <div class="col-md-6  m-4">
-                                    <div class="text-center">
-                                        <h1>Contactez-nous ! </h1>
-                                    </div>
-                                    <form action="contacter_traitement.php" method="POST">
-                                        <div class="form-group">
-                                            <div class="py-3 ">
-                                                <input type="text" name="surname" placeholder="Nom" autocomplete="off" class="form-control rounded" />
-                                            </div>
-                                            <div class="py-3">
-                                                <input type="text" name="firstname" placeholder="Prénom" autocomplete="off" class="form-control rounded" />
-                                            </div>
-                                            <div class="py-3">
-                                                <input type="email" name="email" placeholder="Email" autocomplete="off" class="form-control rounded" />
-                                            </div>
-                                            <div class="py-3">
-                                                <textarea rows="10" name="message" placeholder="Envoyez votre message à bellezafrance.contact@gmail.com" class="form-control rounded"></textarea>
-                                            </div>
-                                           
-                                            <button type="submit" class="btn btn-lg btn-primary rounded ">Envoyer</button>
-                                        </div>
-                                    </form>
-                                </div>
+                            <div class="">
+                                <h1 class="d-flex justify-content-center pt-3">Contactez-nous ! </h1>
                             </div>
+                                <form action="contacter_traitement.php" method="POST">
+                                    <div class="form-group">
+                                         <div class="py-3 ">
+                                        <input type="text" name="surname" placeholder="Nom" autocomplete="off" class="form-control " />
+                                        </div>
+                                        <div class="py-3">
+                                             <input type="text" name="firstname" placeholder="Prénom" autocomplete="off" class="form-control " />
+                                        </div>
+                                         <div class="py-3">
+                                             <input type="email" name="email" placeholder="Email" autocomplete="off" class="form-control " />
+                                        </div>
+                                        <div class="py-3">
+                                             <textarea rows="10" name="message" placeholder="Envoyez votre message à bellezafrance.contact@gmail.com" class="form-control "></textarea>
+                                        </div>
+                                           
+                                        <button type="submit" class="btn btn-lg btn-primary my-3 ">Envoyer</button>
+                                    </div>
+                                </form>
                         </div>
 
                     </div> <!-- fin col-2  -->
